2e75b9137f47a664a9af6d9ee5f045083b39fac6
Phurba Sherpa <srpphurba@gmail.com>
2019-12-04 17:13:35 -0500

    revert

--- js/AddReview.js
+++ js/AddReview.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { TextInput, StyleSheet, View, Button } from 'react-native';
-import { addReviewThunk } from '../client/store/reviews';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { TextInput, StyleSheet, View, Button } from "react-native";
+import { addReviewThunk } from "../client/store/reviews";
+import { connect } from "react-redux";
 
 class AddReview extends Component {
   constructor(props) {
     super();
     this.state = {
-      content: '',
-      ratings: '',
+      content: "",
+      ratings: ""
     };
   }
 
@@ -18,27 +18,27 @@ class AddReview extends Component {
         <TextInput
           value={this.state.content}
           onChangeText={text => this.setState({ content: text })}
-          placeholder={'Comment'}
+          placeholder={"Comment"}
           placeholderTextColor="black"
           style={styles.box}
         />
         <TextInput
           value={this.state.ratings}
           onChangeText={text => this.setState({ ratings: text })}
-          placeholder={'Rating (number btwn 1-5)'}
+          placeholder={"Rating (number btwn 1-5)"}
           placeholderTextColor="black"
           style={styles.box}
         />
         <Button
           title="SUBMIT"
-          style={{ marginTop: '10%' }}
+          style={{ marginTop: "10%" }}
           onPress={() => {
             this.props.addReviewThunk(
               this.props.id,
               this.state.content,
               this.state.ratings
             );
-            this.setState({ content: '', ratings: '' });
+            this.setState({ content: "", ratings: "" });
             this.props.removeReviewBox();
           }}
         />
@@ -50,28 +50,28 @@ class AddReview extends Component {
 let styles = StyleSheet.create({
   input: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-    position: 'absolute',
-    marginTop: 250,
-    width: '100%',
-    marginLeft: 6,
-    height: '30%',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff"
+    // position: "absolute"
+    // marginTop: 250,
+    // width: '100%',
+    // marginLeft: 6,
+    // height: '30%',
   },
   box: {
-    marginTop: '5%',
-    fontSize: 24,
-  },
+    // marginTop: '5%',
+    // fontSize: 24,
+  }
 });
 
 const mapStateToProps = state => ({
-  id: state.businessReducer.id,
+  id: state.businessReducer.id
 });
 
 const mapDispatchToProps = dispatch => ({
   addReviewThunk: (id, content, ratings) =>
-    dispatch(addReviewThunk(id, content, ratings)),
+    dispatch(addReviewThunk(id, content, ratings))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddReview);
--- js/SingleMarker.js
+++ js/SingleMarker.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import { ViroText, ViroImage, ViroFlexView } from 'react-viro';
-import Details from './Details';
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import { ViroText, ViroImage, ViroFlexView } from "react-viro";
+import Details from "./Details";
 
 class SingleMarker extends Component {
   constructor(props) {
@@ -32,27 +32,27 @@ class SingleMarker extends Component {
   }
 
   render() {
-    console.log('PROPS', this.props);
+    console.log("PROPS", this.props);
     let icon =
-      this.props.icon === '/res/city.png'
-        ? require('../js/res/city.png')
-        : this.props.icon === '/res/baricon.png'
-        ? require('../js/res/baricon.png')
-        : this.props.icon === '/res/entertainment-icon-png-14.jpg'
-        ? require('../js/res/entertainment-icon-png-14.jpg')
-        : this.props.icon === '/res/food.png'
-        ? require('../js/res/food.png')
+      this.props.icon === "/res/city.png"
+        ? require("../js/res/city.png")
+        : this.props.icon === "/res/baricon.png"
+        ? require("../js/res/baricon.png")
+        : this.props.icon === "/res/entertainment-icon-png-14.jpg"
+        ? require("../js/res/entertainment-icon-png-14.jpg")
+        : this.props.icon === "/res/food.png"
+        ? require("../js/res/food.png")
         : null;
 
     let finalObj = this.pointToAR(this.props.busLat, this.props.busLong);
     console.log(finalObj);
     return (
       <ViroFlexView
-        style={{ flexDirection: 'column' }}
+        style={{ flexDirection: "column" }}
         width={7}
         height={20}
         position={[finalObj.x, 0, finalObj.z]}
-        transformBehaviors={['billboard']}
+        transformBehaviors={["billboard"]}
         onHover={isHovering => {
           // if (!isHovering) {
           //   this.setState({ clicked: false });
@@ -63,27 +63,28 @@ class SingleMarker extends Component {
           <ViroImage
             style={{ flex: 0.05 }}
             width={1}
-            source={require('../js/res/remove.png')}
+            source={require("../js/res/remove.png")}
             onClick={() => {
               this.setState({ clicked: false });
             }}
           />
         ) : null}
         <ViroImage
-          source={require('./res/directions-icon.png')}
+          source={require("./res/directions-icon.png")}
           style={{ flex: 0.05, width: 1, height: 1 }}
           onClick={this.props.mapCall}
         />
         <ViroFlexView
-          style={{ flexDirection: 'column', flex: 0.95, marginTop: '10%' }}
+          style={{ flexDirection: "column", flex: 0.95, marginTop: "10%" }}
           position={[finalObj.x, 2, finalObj.z]}
-          transformBehaviors={['billboard']}
+          transformBehaviors={["billboard"]}
         >
           <ViroFlexView
             onClick={() => {
+              console.log("i was clickecd");
               this.setState({ clicked: true });
             }}
-            style={{ flexDirection: 'row', flex: 0.1 }}
+            style={{ flexDirection: "row", flex: 0.1 }}
             backgroundColor={this.props.color}
           >
             <ViroImage source={icon} style={{ flex: 0.25 }} />
@@ -91,20 +92,20 @@ class SingleMarker extends Component {
             <ViroFlexView
               style={{
                 flex: 0.75,
-                justifyContent: 'center',
-                alignItems: 'center',
+                justifyContent: "center",
+                alignItems: "center"
               }}
             >
               <ViroText
                 text={`${this.props.name}`}
                 // text="HELLO WORLD"
                 style={{
-                  fontFamily: 'Cochin',
+                  fontFamily: "Cochin",
                   fontSize: 25,
-                  fontWeight: 'bold',
-                  color: 'white',
-                  marginTop: '10%',
-                  textAlign: 'center',
+                  fontWeight: "bold",
+                  color: "white",
+                  marginTop: "10%",
+                  textAlign: "center"
                 }}
               />
             </ViroFlexView>
