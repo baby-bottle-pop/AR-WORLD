b540f630ffc588746ed8b46373cdf34487ba8b90
Phurba Sherpa <srpphurba@gmail.com>
2019-12-04 14:00:44 -0500

    added heroku url in place of ip

--- client/store/user.js
+++ client/store/user.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const GOT_USER = "GET_USER";
 const NEW_USER = "NEW_USER";
 
-const newUser = user => ({type: NEW_USER, user})
+const newUser = user => ({ type: NEW_USER, user });
 
 const gotUser = user => {
   return {
@@ -18,11 +18,14 @@ const initialState = {
 
 export const loginThunk = (email, password) => async dispatch => {
   try {
-    let res = await axios.post(`http://172.16.21.190:8080/auth/login`, {
-      //put your comp ip address
-      email,
-      password
-    });
+    let res = await axios.post(
+      `https://ar-server-v2.herokuapp.com/auth/login`,
+      {
+        //put your comp ip address
+        email,
+        password
+      }
+    );
 
     dispatch(gotUser(res.data));
   } catch (error) {
@@ -30,22 +33,29 @@ export const loginThunk = (email, password) => async dispatch => {
   }
 };
 
-export const signUpThunk = (email, password, firstName, lastName) => async dispatch => {
+export const signUpThunk = (
+  email,
+  password,
+  firstName,
+  lastName
+) => async dispatch => {
   try {
-    let res = await axios.post(`http://172.16.21.190:8080/api/user/signup`, {
-      //put your comp ip address
-      email,
-      password,
-      firstName,
-      lastName
-    });
-    let newUser = {email, password}
+    let res = await axios.post(
+      `https://ar-server-v2.herokuapp.com/api/user/signup`,
+      {
+        //put your comp ip address
+        email,
+        password,
+        firstName,
+        lastName
+      }
+    );
+    let newUser = { email, password };
     dispatch(gotUser(newUser));
   } catch (error) {
     return dispatch(gotUser({ error }));
   }
 };
-
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
